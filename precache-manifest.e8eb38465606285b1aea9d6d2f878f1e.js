d6e42d60a5722eb37f7c1f2e557cd63cd49d7a65
kirbihar <kirbihar@publicisgroupe.net>
2020-05-03 23:27:02 +0530

    Updates

---
+++ precache-manifest.e8eb38465606285b1aea9d6d2f878f1e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee0b2a80de089f66810afd5b8bdfe41",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "19a3066b053a06037cbd",
+    "url": "/shoppingcart/static/css/2.1bbe0483.chunk.css"
+  },
+  {
+    "revision": "42c9917e01afc3493e85",
+    "url": "/shoppingcart/static/css/main.1c134125.chunk.css"
+  },
+  {
+    "revision": "19a3066b053a06037cbd",
+    "url": "/shoppingcart/static/js/2.e1e513f2.chunk.js"
+  },
+  {
+    "revision": "d80e90a5a402eda34744c6861150ccad",
+    "url": "/shoppingcart/static/js/2.e1e513f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c9917e01afc3493e85",
+    "url": "/shoppingcart/static/js/main.a875079f.chunk.js"
+  },
+  {
+    "revision": "9fd441dcbdbfa9849854",
+    "url": "/shoppingcart/static/js/runtime-main.b16db001.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shoppingcart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shoppingcart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shoppingcart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shoppingcart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shoppingcart/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ShoppingCartApp/precache-manifest.f221baf9e87fe176071383ade47ba72b.js"
+  "/shoppingcart/precache-manifest.e8eb38465606285b1aea9d6d2f878f1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShoppingCartApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shoppingcart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
