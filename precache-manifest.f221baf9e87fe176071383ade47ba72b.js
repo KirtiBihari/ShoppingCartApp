82058c5feb51481646f5f1ad4ed70e4474dead5e
kirbihar <kirbihar@publicisgroupe.net>
2020-05-03 23:12:19 +0530

    Updates

---
+++ precache-manifest.f221baf9e87fe176071383ade47ba72b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0e3adc907c68bbcfe4f16638475511",
+    "url": "/ShoppingCartApp/index.html"
+  },
+  {
+    "revision": "94e79e9f4bd6b4fc64c2",
+    "url": "/ShoppingCartApp/static/css/2.2ba98093.chunk.css"
+  },
+  {
+    "revision": "8c9541e40de0656cf2fb",
+    "url": "/ShoppingCartApp/static/css/main.1c134125.chunk.css"
+  },
+  {
+    "revision": "94e79e9f4bd6b4fc64c2",
+    "url": "/ShoppingCartApp/static/js/2.58989b1e.chunk.js"
+  },
+  {
+    "revision": "d80e90a5a402eda34744c6861150ccad",
+    "url": "/ShoppingCartApp/static/js/2.58989b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9541e40de0656cf2fb",
+    "url": "/ShoppingCartApp/static/js/main.c588f8bc.chunk.js"
+  },
+  {
+    "revision": "d67a2bc02dce94370fe6",
+    "url": "/ShoppingCartApp/static/js/runtime-main.56ef4c3d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ShoppingCartApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ShoppingCartApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ShoppingCartApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ShoppingCartApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ShoppingCartApp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
